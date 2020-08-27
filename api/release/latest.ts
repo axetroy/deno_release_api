969088f7a3cc35f4d051e4c5ab1cb2f0b1845f3a
axetroy <axetroy.dev@gmail.com>
2020-08-28 03:41:05 +0800

    update

--- api/release/latest.ts
+++ api/release/latest.ts
@@ -3,16 +3,11 @@ import { ServerRequest } from "https://deno.land/std@0.66.0/http/server.ts";
 
 import { getAllVersions } from "../../common.ts";
 
-export async function handler(req: ServerRequest) {
+export default async function handler(req: ServerRequest) {
   const versions = await getAllVersions();
-
-  const headers = new Headers();
-
-  headers.append("content-type", "application/json");
 
   await req.respond({
     status: 200,
     body: versions[0],
-    headers,
   });
 }
--- api/releases.ts
+++ api/releases.ts
@@ -2,14 +2,9 @@
 import { ServerRequest } from "https://deno.land/std@0.66.0/http/server.ts";
 import { getAllVersions } from "../common.ts";
 
-export async function handler(req: ServerRequest) {
-  const headers = new Headers();
-
-  headers.append("content-type", "application/json");
-
+export default async function handler(req: ServerRequest) {
   await req.respond({
     status: 200,
     body: JSON.stringify(await getAllVersions()),
-    headers,
   });
 }
